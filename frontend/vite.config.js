3ca4ae0bb2d84fde669cf60662d7506b0aed9eb5
omkar-fr <omkardahiphale135@gmail.com>
2026-07-22 22:07:56 +0530

    submission prototype

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,8 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
+        target: 'http://127.0.0.1:8000', 
       }
     }
   }
